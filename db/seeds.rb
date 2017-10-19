94b8aaf40a744967c4b3a9ef871837cc956440a7
Niraj Rao <niraj.a.rao@berkeley.edu>
2017-10-19 01:00:46 +0000

    Seeded initial ward (Ward 1)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+wards = Ward.create(name: 1, polygons: [[30.577421, 77.504928], [30.683129, 77.631935], [30.987524, 77.313168], [30.806854, 77.066531]])
