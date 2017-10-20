b5aa10e02cdcefc230eaaa1ad8a6545384b71ac2
Adeel Cheema <acheema@berkeley.edu>
2017-10-20 21:10:27 +0000

    fixing api key

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
   def index
-    Rails.logger.warn("### PUBLIC KEY IS #{ENV['google_api_key'].inspect}")
-    puts ENV['google_api_key']
-    puts "//maps.google.com/maps/api/js?key=" + ENV['google_api_key']
+    @api_key = ENV['google_api_key']
+    puts @api_key
     @coordinates = Coordinate.all
     @marker_hash = Gmaps4rails.build_markers(@coordinates) do |coordinate, marker|
       marker.lat coordinate.lat
