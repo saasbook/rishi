9a95ec5235d20e29340a406b2bb671e98b63a8cb
Niraj Rao <niraj.a.rao@berkeley.edu>
2017-10-26 01:46:09 +0000

    Started to create seed data for Wards, Committees, Coordinates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-ward_1 = Ward.create(name: "Ward 1", color: "blue")
-ward_2 = Ward.create(name: "Ward 2", color: "red")
+ward_1 = Ward.create(name: "Ward 1")
+ward_2 = Ward.create(name: "Ward 2")
 
-Coordinate.create(ward_id: ward_1.id, lat: 30.577421, lng: 77.504928)
-Coordinate.create(ward_id: ward_1.id, lat: 30.683129, lng: 77.631935)
-Coordinate.create(ward_id: ward_1.id, lat: 30.987524, lng: 77.313168)
-Coordinate.create(ward_id: ward_1.id, lat: 30.806854, lng: 77.066531)
-Coordinate.create(ward_id: ward_2.id, lat: 30.797834, lng: 77.361996)
+puts(ward_1.coordinates)
+coords = [Coordinate.create(lat: 30.577421, lng: 77.504928),
+          Coordinate.create(lat: 30.683129, lng: 77.631935),
+          Coordinate.create(lat: 30.987524, lng: 77.313168),
+          Coordinate.create(lat: 30.806854, lng: 77.066531)]
+          
+ward_1.coordinates.create(:lat => 30.577421, :lng =>  77.504928)
+          
