a13d0fc0914c7b5dcdd02a4e8dcac43cafc66a9b
Niraj Rao <niraj.a.rao@berkeley.edu>
2017-10-27 22:55:48 +0000

    Refactored HomeController#Index for marker building and placement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,19 +3,8 @@ class HomeController < ApplicationController
     @api_key = ENV['google_api_key']
     @wards = Ward.all
     @coordinates = Coordinate.all
-    @polygons = []
-    @marker_hash = Gmaps4rails.build_markers(@coordinates) do |coordinate, marker|
-      marker.lat coordinate.lat
-      marker.lng coordinate.lng
-    end
-    @ward_ids = Ward.uniq.pluck(:id)
-    @ward_ids.each do |ward_id|
-      coordinates_for_ward = Coordinate.where(:ward_id => ward_id)
-      @current_ward_hash = Gmaps4rails.build_markers(coordinates_for_ward) do |coordinate, marker|
-        marker.lat coordinate.lat
-        marker.lng coordinate.lng
-      end
-      @polygons.push(@current_ward_hash)
-    end
+    @regions = []
+    @marker_hash = Ward.build_markers
+    @regions = Ward.build_ward_overlay(@regions)
   end
 end
