7039e662813d0199aca05f6bd855c8c2941e226e
Chittaranjan <azxc10@gmail.com>
2017-11-01 19:10:13 -0700

    popups enabled

--- app/models/ward.rb
+++ app/models/ward.rb
@@ -1,16 +1,19 @@
 class Ward < ActiveRecord::Base
     has_many :committees
     has_many :coordinates
-    
+
+
     def self.build_markers
+
       coordinates = Coordinate.all
       marker_hash = Gmaps4rails.build_markers(coordinates) do |coordinate, marker|
         marker.lat coordinate.lat
         marker.lng coordinate.lng
+        marker.infowindow "contentString"
       end
       return marker_hash
     end
-    
+
     def self.build_ward_overlay()
       regions = []
       wards = Ward.all
