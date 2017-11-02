6623fe244166bc65d7ee30490818623a7f2b0f8b
Pranav Harikrishnan <phari@berkeley.edu>
2017-11-02 02:19:50 +0000

    Added overlay for all wards

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,11 @@ class HomeController < ApplicationController
     @wards = Ward.all
     @coordinates = Coordinate.all
     @marker_hash = Ward.build_markers
-    @regions = Ward.build_ward_overlay
+    @regions_1 = Ward.build_ward_overlay(Ward.find_by_name("Ward 1"))
+    @regions_2 = Ward.build_ward_overlay(Ward.find_by_name("Ward 2"))
+    @regions_3 = Ward.build_ward_overlay(Ward.find_by_name("Ward 3"))
+    @regions_4 = Ward.build_ward_overlay(Ward.find_by_name("Ward 4"))
+    @regions_5 = Ward.build_ward_overlay(Ward.find_by_name("Ward 5"))
   end
   
   def about
--- app/models/ward.rb
+++ app/models/ward.rb
@@ -3,25 +3,23 @@ class Ward < ActiveRecord::Base
     has_many :coordinates
     
     def self.build_markers
-      coordinates = Coordinate.all
-      marker_hash = Gmaps4rails.build_markers(coordinates) do |coordinate, marker|
-        marker.lat coordinate.lat
-        marker.lng coordinate.lng
+      wards = Ward.all
+      #coordinates = Coordinate.all
+      marker_hash = Gmaps4rails.build_markers(wards) do |ward, marker|
+        marker.lat ward.ward_lat
+        marker.lng ward.ward_long
       end
       return marker_hash
     end
     
-    def self.build_ward_overlay()
+    def self.build_ward_overlay(ward)
       regions = []
-      wards = Ward.all
-      wards.each do |ward|
         ward_coordinates = ward.coordinates
         @ward_marker_hash = Gmaps4rails.build_markers(ward_coordinates) do |coordinate, marker|
           marker.lat coordinate.lat
           marker.lng coordinate.lng
         end
         regions.push(@ward_marker_hash)
-      end
       return regions
     end
     
