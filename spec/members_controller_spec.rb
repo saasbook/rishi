dd097114d923531f475551d3d02243ead6135cf2
Adeel Cheema <acheema@berkeley.edu>
2017-11-17 21:49:29 +0000

    [Feature] Added logged in check before member log-in
    To access the members, page, one must be logged in

--- spec/members_controller_spec.rb
+++ spec/members_controller_spec.rb
@@ -3,9 +3,15 @@ require "rails_helper"
 
 describe MembersController , :type => :controller do
   describe "GET index" do
-    it "renders the index template" do
+    it "renders the index template when logged in" do
+      @request.session[:user_id] = "fake user"
       get :index
-    #   expect(response).to render_template("index")
+      expect(response).to render_template("index")
+    end
+    it "deos not render the index template when not logged in" do
+      @request.session[:user_id] = nil
+      get :index
+      expect(response).to redirect_to(root_path)
     end
   end
 end
